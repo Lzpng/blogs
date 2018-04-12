a72183b60c960789f4babf7d725c3f80119e0c62
keysaim <keysaim@gmail.com>
2018-04-12 14:00:20 +0800

    Split the deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -16,21 +16,5 @@ if [ $? -ne 0 ]; then
     echo "Push failed"
 fi
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+./io.sh "$msg"
 
-# Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
-
-# Go To Public folder
-cd public
-# Add changes to git.
-git add .
-
-# Commit changes.
-git commit -m "$msg"
-
-# Push source and build repos.
-git push origin master
-
-# Come Back up to the Project Root
-cd ..
